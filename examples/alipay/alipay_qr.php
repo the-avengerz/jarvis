dc7053a66d09db59666d6e0794b351b2e32a7d65
JanHuang <bboyjanhuang@gmail.com>
2018-05-12 00:46:56 +0800

    add examples

---
+++ examples/alipay/alipay_qr.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2018
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      http://www.fast-d.cn/
+ */
+
+use Avengers\Jarvis\Jarvis;
+
+include __DIR__ . '/../../vendor/autoload.php';
+
+$config = include __DIR__.'/config.php';
+
+$jarvis = new Jarvis(Jarvis::JARVIS_ALIPAY_WEB, $config);
+
+
+$jarvis->enableDebug();
+
+$form = $jarvis->charge([
+    'order_id' => '151627101400000071',
+    'subject' => 'testing',
+    'amount' => '0.01',
+    'currency' => 'CNY',
+    'description' => 'testing description',
+    'return_url' => 'https://www.baidu.com',
+//    'expired_at' => '2018-06-23 19:00:00',
+]);
+
+echo '<pre>';
+print_r($form);
---
+++ examples/alipay/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2018
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      http://www.fast-d.cn/
+ */
+
+
+// 该APP KEY仅仅作为测试使用。
+return [
+    'app_id' => '2016082000300779',
+    'private_key' => __DIR__ . '/rsa_private_key.pem',
+    'public_key' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtEmmb37ZVKZM0Etea2uuHVT5+4y3IextGRECzseeH0+HsjAF/W1D3aGCiNzJejopbGEsY0fXaT+pElqq/UK6qDzFcqOPftNnhOPV4mfbRd0XQWDecYDIoR74MlGrsUTpvIaBb6uGqm66T4e65SxPaPjfO51Tp2Fl/B7Ldmxnl2CEp97u+ZA8ZPvNldlGScSg9o06rt6A9cfR6T9uHZ9FOBv/7H7ev3pE6lMb9bP+X+sl2gTmi+7v12+HImMYcnZLIRwTuPJkcJEnVL6/0PmOsWm2t7ZIqFl7mMtBEV22tnSLw3K6gb7qQseiMrkDtg/4MA/Uzrka7JX/oHn24XCBkwIDAQAB',
+    'notify_url' => 'http://baidu.com/'
+];
